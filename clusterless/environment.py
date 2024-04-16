4f250a8aa7d1ced94346321df66ee11669345c0f
LSaldyt <lucassaldyt@gmail.com>
2024-04-15 17:48:35 -0700

    Large refactor to map/environment

--- clusterless/environment.py
+++ clusterless/environment.py
@@ -1,63 +1,27 @@
 import numpy as np
 import numpy.typing as npt
 from dataclasses import dataclass
+from . import utils
+
+from .map import Map
 
 @dataclass
 class Memory():
     grid : npt.ArrayLike
     time : npt.ArrayLike
 
-from . import utils
-def create_grid(gen, grid_shape, probs, agent_index=3):
-    ''' Create an initial environment from a multinomial distribution '''
-    grid = gen.choice(np.arange(len(probs)), size=grid_shape, p=list(probs.values()))
-    # Re-assign agents to unique numbers, e.g. [3, 3, 3] becomes [3, 4, 5]
-    mask = grid == agent_index
-    size = grid_shape[0]
-
-    grid[mask] = (np.arange(np.sum(mask)) + agent_index)
-    coordinates = utils.cartesian_product(np.arange(size), np.arange(size))
-    return grid, coordinates
-
-def render(grid, syms):
-    ''' Render the environment in questionable unicode '''
-    h, w = grid.shape
-    def gen_syms():
-        for i in range(h):
-            for j in range(w):
-                yield syms[grid[i, j]]
-            yield '\n'
-    return ''.join(gen_syms())
-
-def full_render(grid, sense_input, s):
-    rendered_views = [(' ' * s.view_size + '\n') * s.view_size]
-    rendered_grids = [render(grid, s.symbols)]
-    codes          = []
-    for c, view, mem, coords in sense_input:
-        rendered_views.append(render(view,     s.symbols))
-        rendered_grids.append(render(mem.grid, s.symbols))
-        codes.append(f'agent {s.symbols[c]}')
-    descriptions   = [f'{name:<{s.size}}\n' for name in ['full'] + codes]
-    print(utils.horizontal_join(rendered_grids))
-    print(utils.horizontal_join(descriptions))
-    print(utils.horizontal_join(rendered_views, join=' ' * (s.size - s.view_size + 1)))
-
-def set_at(grid, coords, values):
-    ''' Set grid by vectorized coordinates to new values '''
-    grid[coords[:, 0], coords[:, 1]] = values
-
-def transition(grid, actions, agent_coords, agent_codes, codes):
-    ''' Note: This function MODIFIES grid intentionally, for efficiency.
+def transition(map, actions, agent_coords, agent_codes, codes):
+    ''' Note: This function MODIFIES map intentionally, for efficiency.
 
         Move all agents at once according to their actions:
         Agent actions can be stay, up, down, left, and right
         If an agent touches a goal, that goal is achieved. 
         Agents cannot step into obstacles.. or they stay in place at a penalty 
         Agents cannot collide.. or they die? ☠'''
-    # Progress actions, (optionally) enforce grid boundaries
+    # Progress actions, (optionally) enforce map boundaries
     next_coords    = (agent_coords + actions) 
-    next_coords    = next_coords % (grid.shape[0])
-    next_locations = grid[next_coords[:, 0], next_coords[:, 1]]
+    next_coords    = next_coords % (map.grid.shape[0])
+    next_locations = map.grid[next_coords[:, 0], next_coords[:, 1]]
     # Enforce obstacles (and dead agents as obstacles)
     allowed_move   = ((next_locations != codes['obstacle']) & (next_locations != codes['dead']))
     final_coords   = np.where(utils.broadcast(allowed_move, 2), next_coords, agent_coords)
@@ -66,12 +30,12 @@ def transition(grid, actions, agent_coords, agent_codes, codes):
     collision_mask   = unique_counts > 1
     collision_coords = unique_coords[collision_mask]
     if (collision_mask).any():
-        set_at(grid, collision_coords, codes['dead'])
+        map.set_at(collision_coords, codes['dead'])
     non_collision_coords = unique_coords[collision_mask == False]
-    reached_locations    = grid[non_collision_coords[:, 0], non_collision_coords[:, 1]]
+    reached_locations    = map.grid[non_collision_coords[:, 0], non_collision_coords[:, 1]]
     # Move agents to (filtered) locations
-    set_at(grid, agent_coords, codes['empty'])
-    set_at(grid, final_coords, agent_codes)
+    map.set_at(agent_coords, codes['empty'])
+    map.set_at(final_coords, agent_codes)
 
     # Count goals and collision types
     goal_mask = reached_locations == codes['goal']
@@ -81,22 +45,22 @@ def transition(grid, actions, agent_coords, agent_codes, codes):
         n_collisions_agents   = np.sum(unique_counts) - unique_counts.shape[0],
     )
 
-def views(grid, agent_coords, s):
+def views(map, agent_coords, s):
     ''' Produce local views for all agents.
-        A view is a local n x n grid around an agent.
-        View that go beyond the border of the grid are padded. '''
+        A view is a local n x n map around an agent.
+        View that go beyond the border of the map are padded. '''
     n_agents = agent_coords.shape[0]
     view_box = utils.box(s.view_size)
     for i in range(n_agents): # Seemed intuitive, faster and more memory efficient than a vectorized operation
         view_coords    = view_box + agent_coords[i, :]
-        view_coords    = view_coords % grid.shape[0]
-        view           = grid[view_coords[:, 0], view_coords[:, 1]]
+        view_coords    = view_coords % map.grid.shape[0]
+        view           = map.grid[view_coords[:, 0], view_coords[:, 1]]
         yield agent_coords[i, :], view_coords, view.reshape((s.view_size, s.view_size))
 
-def get_agents(grid, coordinates, codes):
-    mask     = grid >= codes['agent']
-    codes    = grid[mask]
-    coords   = coordinates[mask.reshape((np.prod(grid.shape),))]
+def get_agents(map, s):
+    mask     = map.grid >= s.codes['agent']
+    codes    = map.grid[mask]
+    coords   = map.coords_of(mask)
     n_agents = np.sum(mask)
     return codes, coords, n_agents
 
@@ -105,13 +69,13 @@ def get_agents(grid, coordinates, codes):
     otherwise empty/obstacle are certain, 
     agents/goals in current time step are certain '''
 
-def init_memory(grid, agent_codes, codes):
-    return {k : Memory(np.full(grid.shape, codes['unseen']), 
-                       np.full(grid.shape, 0)) 
+def init_memory(map, agent_codes, codes):
+    return {k : Memory(np.full(map.grid.shape, codes['unseen']), 
+                       np.full(map.grid.shape, 0)) 
             for k in agent_codes}
 
-def sense_environment(grid, memory, agent_codes, agent_coords, s, timestep):
-    for c, (ac, view_coords, view) in zip(agent_codes, views(grid, agent_coords, s)): # Important: views should be taken before transitions for consistency
+def sense_environment(map, memory, agent_codes, agent_coords, s, timestep):
+    for c, (ac, view_coords, view) in zip(agent_codes, views(map, agent_coords, s)): # Important: views should be taken before transitions for consistency
         memory[c].grid[view_coords[:, 0], view_coords[:, 1]] = view.ravel() 
         memory[c].time[view_coords[:, 0], view_coords[:, 1]] = timestep
         yield c, view, memory[c], ac
@@ -119,4 +83,30 @@ def sense_environment(grid, memory, agent_codes, agent_coords, s, timestep):
 def merge_memory(mem_a, mem_b):
     return np.where(mem_a.time < mem_b.time, mem_a.grid, mem_b.grid)
 
-  
+def simulate(map, policy, s, do_render=False): 
+    score   = 0 # Number of goals achieved
+    n_goals = map.count('goal')
+
+    agent_codes, agent_coords, n_agents = get_agents(map, s)
+    memory = init_memory(map, agent_codes, s.codes)
+
+    step_count = s.timesteps
+    for t in range(s.timesteps):
+        agent_codes, agent_coords, n_agents = get_agents(map, s)
+        sense_input = list(sense_environment(map, memory, agent_codes, agent_coords, s, t))
+
+        if do_render:
+            map.full_render(sense_input, s)
+
+        actions = policy(s, n_agents, sense_input, map.coordinates)
+        info    = transition(map, actions, agent_coords, agent_codes, s.codes) # Important: Do transition at the end of the loop
+
+        score += info['n_goals_achieved']
+        info.update(score=score, n_goals=n_goals)
+        remaining_goals = map.count('goal')
+        if remaining_goals == 0 or n_agents == 0:
+            sense_input = list(sense_environment(map, memory, agent_codes, agent_coords, s, t))
+            step_count = t if remaining_goals==0 else s.timesteps
+            break
+    assert score <= n_goals
+    return dict(score=score, percent=score/n_goals, step_count=step_count)
--- experiments/experiment_classes/base.py
+++ experiments/experiment_classes/base.py
@@ -1,5 +1,5 @@
 from science import Experiment
-from clusterless.environment import create_grid, render, transition, get_agents, init_memory, sense_environment, full_render
+from clusterless.environment import Map, simulate 
 from clusterless.policies    import available_policies
 
 import numpy as np
@@ -10,36 +10,10 @@ class BaseExperiment(Experiment):
         s      = self.settings     # Shorthand
         policy = available_policies[s.policy]
 
-        grids = [(i_r,) + create_grid(s.gen, (s.size, s.size), s.probs)
+        grids = [(i_r, Map(s))
                  for i_r in range(s.environment_samples)]
 
-        for i_r, grid, coordinates in grids:
-            score   = 0 # Number of goals achieved
-            n_goals = np.sum(grid == s.codes['goal'])
-
-            agent_codes, agent_coords, n_agents = get_agents(grid, coordinates, s.codes)
-            memory = init_memory(grid, agent_codes, s.codes)
-
-            step_count = s.timesteps
-            for t in range(s.timesteps):
-                agent_codes, agent_coords, n_agents = get_agents(grid, coordinates, s.codes)
-                sense_input = list(sense_environment(grid, memory, agent_codes, agent_coords, s, t))
-
-                full_render(grid, sense_input, s)
-
-                actions = policy(s, n_agents, sense_input, coordinates)
-                info    = transition(grid, actions, agent_coords, agent_codes, s.codes) # Important: Do transition at the end of the loop
-
-                score += info['n_goals_achieved']
-                info.update(score=score, env_index=i_r, n_goals=n_goals)
-                print(f'Step {t}: {info}')
-                self.log(f'run_{i_r}', info)
-                print('-' * 80)
-                remaining_goals = np.sum(grid == s.codes['goal'])
-                if remaining_goals == 0 or n_agents == 0:
-                    print(f"goals left: {remaining_goals}, agents left: {n_agents}")
-                    sense_input = list(sense_environment(grid, memory, agent_codes, agent_coords, s, t))
-                    step_count = t if remaining_goals==0 else s.timesteps
-                    break
-            assert score <= n_goals, f'{info}'
-            self.log(f'summary', dict(score=score, percent=score/n_goals, step_count=step_count, env_index=i_r))
+        for i_r, grid in grids:
+            stats = simulate(grid, policy, s, do_render=True)
+            stats.update(environment_index=i_r)
+            self.log(f'summary', stats)
