19fa7ebd2f1c994d9c190389528977a311566da4
kstechly <kayastechly@gmail.com>
2024-04-16 09:34:47 -0700

    analyze now finds latest experiment that satisfies arbitrary flags

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,10 +1,27 @@
 import pandas as pd
 from pathlib import Path
+import json
+from functools import reduce
 
-def run(exp='env_8'):
+def run(exp='env_8', **kwargs):
     parent = Path('data/experiments')
     exp    = parent / exp
-    latest = [p for p in exp.iterdir()][-1]
-    df = pd.read_csv(latest / 'data/summary.csv')
+    experiments = [p for p in exp.iterdir()]
+    latest = sorted(experiments)[::-1]
+    stamp = ''
+    print(kwargs)
+    for x in latest: 
+        with (x/'meta.json').open() as f:
+            meta = json.load(f)
+        if kwargs: acceptable = reduce(lambda x,k: x and k in meta.keys() and meta[k]==dict(kwargs)[k],dict(kwargs),True)
+        else: acceptable=True
+        if acceptable:
+            stamp = x
+            break  
+    if not stamp:
+        print(f"data for {kwargs} not found")
+        exit()
+    print(stamp)
+    df = pd.read_csv(stamp / 'data/summary.csv')
     print(df)
     print(df.describe())
